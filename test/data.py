8b2f5f229bbce404fba2fa4eb36525c4138b89b2
AJ <alexander.renold@gmail.com>
2014-03-01 13:19:05 -0800

    added books to testing data

---
+++ test/data.py
@@ -0,0 +1,37 @@
+books = [
+    { 'title': "Super Sad True Love Story",
+      'author': "Gary Shteyngart",
+      'publisher': "Random House",
+      'genres': ["Fiction","Romance"],
+      'cover_large': "static/images/super_sad_true_love_story.large.jpg",
+      'cover_thumb': "static/images/super_sad_true_love_story.thumb.jpg",
+    },
+    { 'title': "Milk",
+      'author': "Anne Mendelson",
+      'publisher': "Knopf",
+      'genres': ["Non-Fiction", "Food"],
+      'cover_large': "static/images/milk.large.jpg",
+      'cover_thumb': "static/images/milk.thumb.jpg"
+    },
+    { 'title': "Winchell",
+      'author': "Neal Gabler",
+      'publisher': "Vintage",
+      'genres': ["Non-Fiction", "Biographies"],
+      'cover_large': "static/images/winchell.large.jpg",
+      'cover_thumb': "static/images/winchell.thumb.jpg",
+    },
+    { 'title': "Get Used To The Seats",
+      'author': "Students Of Cardozo And Woodrow Wilson High Schools",
+      'publisher': "826DC",
+      'genres': ["Non-Fiction"],
+      'cover_large': "static/images/get_used_to_the_seats.large.jpg",
+      'cover_thumb': "static/images/get_used_to_the_seats.thumb.jpg"
+    },
+    { 'title': "Adland",
+      'author': "James P. Othmer",
+      'publisher': "Anchor",
+      'genres': ["Non-Fiction", "Advertising"],
+      'cover_large': "static/images/adland.large.jpg",
+      'cover_thumb': "static/images/adland.thumb.jpg"
+    }
+]
