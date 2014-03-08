dab54e9b554e2beb6ed13645eef7a40d4df55117
AJ <alexander.renold@gmail.com>
2014-03-08 13:57:14 -0800

    added more detailed author model and updated authorpage.html

--- models.py
+++ models.py
@@ -40,7 +40,7 @@ class Book(db.Model):
         self.cover_thumb = cover_thumb
 
     @staticmethod
-    def book_from_kwargs(**kwargs):
+    def book_from_dict(**kwargs):
         return Book(kwargs.get('title', ""),
                     kwargs.get('author', ""),
                     kwargs.get('publisher', ""),
@@ -56,12 +56,29 @@ class Author(db.Model):
 
     author_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
+    bio = db.Column(db.String, nullable=False)
+    picture = db.Column(db.String, nullable=False)
+    website = db.Column(db.String, nullable=False)
+    blog = db.Column(db.String, nullable=False)
     books = db.relationship('Book', backref='author',
                                 lazy='dynamic')
 
-
-    def __init__(self, name):
+    def __init__(self, name, bio, picture, website, blog, twitter_id):
         self.name = name
+        self.bio = bio
+        self.picture = picture
+        self.website = website
+        self.blog = blog
+        self.twitter_id = twitter_id
+
+    @staticmethod
+    def author_from_dict(**kwargs):
+        return Author(kwargs.get('name', ""),
+                      kwargs.get('bio', ""),
+                      kwargs.get('picture', ""),
+                      kwargs.get('website', ""),
+                      kwargs.get('blog', ""),
+                      kwargs.get('twitter_id', ""))
 
     def __repr__(self):
         return '<author {}>'.format(self.name)
