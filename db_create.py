495e209cb5ef42f40c286ac0be0205815be4df9b
AJ <alexander.renold@gmail.com>
2014-03-08 12:06:29 -0800

    added author model and route for /author

--- db_create.py
+++ db_create.py
@@ -2,16 +2,22 @@ __author__ = 'ajrenold'
 
 # db_create.py
 
-from test.data import books
-from models import db, Flaskr, Book
+from test.data import books, authors
+from models import db, Flaskr, Book, Author
 
 def createTestDB(db):
 
     # create the database and the db table
     db.create_all()
 
-    # load testing data
+    # load authors
+    for author_data in authors:
+        db.session.add(Author(author_data['name']))
+    db.session.commit()
+
+    # load books
     for book_data in books:
+        book_data['author'] = Author.query.filter_by(name=book_data['author']).first()
         db.session.add(Book.book_from_kwargs(**book_data))
     # commit the changes
     db.session.commit()
