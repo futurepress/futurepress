c2e18678fc764a56849406595b2bf7c8b254a7ec
AJ <alexander.renold@gmail.com>
2014-05-06 15:40:25 -0700

    removd db.create_all from wsgi

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ from test.db_bootstrap import bootstrapTestDB
 
 create_app('core.AWSConfig')
 
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+#    db.create_all()
 
 application = app
