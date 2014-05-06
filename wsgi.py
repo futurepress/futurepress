81a768a817924a44ee9e4f7b5765f435db3bb682
AJ <alexander.renold@gmail.com>
2014-05-06 15:34:57 -0700

    updated wsgi.py to remove bootstrapping db step

--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,7 @@ from test.db_bootstrap import bootstrapTestDB
 
 create_app('core.AWSConfig')
 
-if not os.path.isfile("aws.db"):
-    print "aws.db not found, creating..."
-    with app.app_context():
-        db.create_all()
-        bootstrapTestDB(db)
+with app.app_context():
+    db.create_all()
 
 application = app
