554a0653e13b0ecd1c80272a8f91712846065563
AJ <alexander.renold@gmail.com>
2014-05-06 13:20:27 -0700

    updated wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 
 from migrate.versioning import api
+import os
 
 from core import AWSConfig
-from application import app, create_app
-
+from application import app, create_app, db
+from test.db_bootstrap import bootstrapTestDB
 
 create_app('core.AWSConfig')
 
@@ -12,6 +13,8 @@ if not os.path.isfile("aws.db"):
     with app.app_context():
         db.create_all()
         bootstrapTestDB(db)
+
+application = app
 
 #api.upgrade(AWSConfig.SQLALCHEMY_DATABASE_URI, AWSConfig.SQLALCHEMY_MIGRATE_REPO)
 #print 'Current database version: ' + str(api.db_version(DevConfig.SQLALCHEMY_DATABASE_URI, DevConfig.SQLALCHEMY_MIGRATE_REPO))
